e8f343c95a8317176d014bd652fb4ad14b5cb722
BnDLett <73402249+BnDLett@users.noreply.github.com>
2023-12-25 15:32:58 -0500

    Updated to 1.3.0a
    Switched from DearPyGUI to PyQT6.

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
+import sys
+
 import yt_dlp, threading, queue, colorama, os, time, argparse
-import dearpygui.dearpygui as dpg
+from PyQt6.QtWidgets import *
 
 from pygame import mixer
+
 mixer.init()
 
-version = "1.2.1a"
-
-"""
-PyMedia from Weebed-Coder
-
-This is an alpha version of PyMedia 1.2.0, do expect bugs and missing features.
-"""
+version = "1.3.0a"
 
 ydl_opts = {
     'format': 'bestaudio/best',
     'quiet': True,
     'outtmpl': "cache/%(title)s-%(id)s.%(ext)s",
-    "paths": {'mp3':'cache', 'webm':'cache'},
+    "paths": {'mp3': 'cache', 'webm': 'cache'},
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -44,31 +41,33 @@ Flags:
  -nocache - Will delete the .wav file once it is done playing, this is useful for when you're minimizing disk space usage.
    Usage: play [link] -nocache
    Aliases: -n
-""" 
-# I understand this may be bad practice however for now I'll 
-# keep it this way, simplifies the program. (aka am 
-# too lazy to make a more convoluted solution)
+"""  # I understand this may be bad practice however for now I'll keep it this way, it simplifies the program.
 
 global audio_process
 global audio_queue
 global audio_thread
+audio_process = mixer.Channel
 audio_thread = False
 audio_queue = queue.Queue()
 
-def download_audio(user_input: str):
+
+def download_audio(link: str):
     """
-    Downloads audio using yt_dlp library
+    Downloads audio using the YT-DLP library.
+    :param link: The link to the audio to be downloaded.
+    :return:
     """
     print("Loading audio... this may take a moment.")
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        if user_input.startswith("https://") == False and user_input.startswith("www.") == False:
-            funny_dict = ydl.extract_info(f"ytsearch:{user_input}", download=False)['entries'][0]['id']
-            user_input = f"https://youtube.com/watch?v={funny_dict}"
-        elif user_input.startswith("http://"):
-            print("Visiting http websites is unsafe, please refrain from visiting websites using http and not https as they're insecure.")
+        if link.startswith("https://") == False and link.startswith("www.") == False:
+            funny_dict = ydl.extract_info(f"ytsearch:{link}", download=False)['entries'][0]['id']
+            link = f"https://youtube.com/watch?v={funny_dict}"
+        elif link.startswith("http://"):
+            print("Visiting http websites is unsafe, please refrain from visiting websites using http and not https as "
+                  "they're insecure.")
 
-        info = ydl.extract_info(user_input, False)
+        info = ydl.extract_info(link, False)
         info = ydl.prepare_filename(info)
 
         print(info)
@@ -78,18 +77,20 @@ def download_audio(user_input: str):
         else:
             print(1)
             info = info.replace(info[-4:], ".mp3")
-        
+
         print(info)
 
-        if os.path.exists(info): # This is for caches (if any)
+        if os.path.exists(info):  # This is for caches (if any)
             return info
-        ydl.download([user_input])
+        ydl.download([link])
 
     return info
 
+
 def start_audio():
     """
-    Starts the audio thread, alongside handling removing items from queue.
+    Starts the audio thread and deletes audio cache if specified.
+    :return:
     """
 
     global audio_thread
@@ -106,40 +107,49 @@ def start_audio():
 
         global audio_process
         global paused
-        #audio_wavobj = simpleaudio.WaveObject.from_wave_file(file[0])
-        #audio_process = audio_wavobj.play()
-        #audio_process.wait_done()
+        # audio_wavobj = simpleaudio.WaveObject.from_wave_file(file[0])
+        # audio_process = audio_wavobj.play()
+        # audio_process.wait_done()
         tada = mixer.Sound(file[0])
         audio_process = tada.play()
         paused = False
         while audio_process.get_busy():
             time.sleep(0.1)
-        if file[1]: os.remove(f"cache/{file}")
+        if file[1]: os.remove(f"{file[0]}")
+
 
 def attempt_clear():
+    """
+    Attempts to clear the terminal. Does not work for macOS.
+    :return:
+    """
     try:
         os.system("clear")
-    except Exception as hi_neko: # This is not tested on a windows machine yet.
+    except Exception as hi_neko:  # This is not tested on a Windows machine yet.
         os.system("cls")
 
-def prepare_and_play(sender = False, user_input: str = None, del_cache: bool = True):
+
+def prepare_and_play(link: str = None, del_cache: bool = True):
     """
-    Prepares and plays the audio.
+    Prepares and plays audio using YT-DLP and PyGame Mixer.
+    :param link: The link for the audio to be downloaded.
+    :param del_cache: If audio cache should be deleted or not.
+    :return:
     """
 
-    if sender:
-        user_input = dpg.get_value("user_url")
-    print(user_input)
+    print(link)
     global audio_thread
-    file_loc = download_audio(user_input) 
+    file_loc = download_audio(link)
     audio_queue.put([file_loc, del_cache])
-    if audio_thread == False:
-        audio_thread = threading.Thread(target = start_audio, name = "PyMedia Audio Player")
+    if not audio_thread:
+        audio_thread = threading.Thread(target=start_audio, name="PyMedia Audio Player")
         audio_thread.start()
+
 
 def user_interface():
     """
     CLI user interface
+    :return:
     """
 
     while True:
@@ -159,7 +169,7 @@ def user_interface():
             print(help_info)
 
         elif user_input[0].lower() in ["play", "start", "queue"]:
-            prepare_and_play(user_input = user_input[1], del_cache = del_cache)
+            prepare_and_play(user_input=user_input[1], del_cache=del_cache)
 
         elif user_input[0].lower() in ["pause"]:
             audio_process.pause()
@@ -169,70 +179,93 @@ def user_interface():
 
         elif user_input[0].lower() in ["stop"]:
             audio_process.stop()
-        
+
         elif user_input[0].lower() in ["quit", "exit", "leave"]:
             exit()
-        
+
         if clear:
             attempt_clear()
 
-def gui_interface():
+
+class GUIInterface(QMainWindow):
     """
-    GUI user interface
+    GUI interface
+    :return:
     """
 
-    def change_pause_state():
-        try:
-            global audio_process
-            global paused
-            if paused == False: 
-                audio_process.pause()
-                paused = True
-            else: 
-                audio_process.unpause()
-                paused = False
-        except Exception as err:
-            raise err
-    
-    # These are here to bypass some issues in DearPyGUI
-    def move_along_now():
-        audio_process.stop()
-    def close_app(_sender, _data):
-        os._exit(0)
+    def __init__(self, version):
+        super().__init__()
 
-    dpg.create_context()
-    dpg.create_viewport()
+        global audio_process
+        global paused
+        paused = False
 
-    with dpg.window(label=f"PyMedia v{version}", tag="Primary Window"):
-        # Note: Will need to add a bottom border for :sparkles:style:sparkles:
-        with dpg.group(label="search_upper", horizontal=True):
-            user_url = dpg.add_input_text(label="URL", tag="user_url")
-            dpg.add_button(label="Confirm", callback=prepare_and_play)
-            dpg.add_button(label="Exit", callback=close_app) # This is here due to a minor issue in DearPyGUI
-            
-        with dpg.group(label="main_middle"):
-            pass # This section will contain locally installed files that can use
-        with dpg.group(label="media_controls"):
-            with dpg.group(label="playback_control", horizontal=True):
-                dpg.add_button(label = "<") # Will need to round these out later when possible, also the button "<" will not work because there is no functionality for it yet
-                dpg.add_button(label = "||", callback = change_pause_state)
-                dpg.add_button(label = ">", callback = move_along_now)
+        self.__audio_process__ = audio_process
+        self.__paused__ = paused
+        self.__version__ = version
+        self.spacing = 5
 
-    dpg.create_viewport(title=f'PyMedia v{version}', width=600, height=200)
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-    dpg.set_primary_window("Primary Window", True)
-    dpg.start_dearpygui()
-    dpg.destroy_context()
+        self.setWindowTitle(f"PyMedia {self.__version__}")
+        self.setGeometry(0, 0, 600, 200)
+
+        self.link = QLineEdit("", self)
+        self.link.setGeometry(5, 5, 325, 25)
+
+        enter = QPushButton("Enter", self)
+        enter.clicked.connect(self.run_audio)
+        enter.setGeometry((self.link.x() + self.link.width()) + self.spacing, self.link.y(), 50, self.link.height())
+
+        previous_btn = QPushButton("Previous", self)
+        previous_btn.setGeometry(self.link.x(), (self.link.y() + self.link.height()) + self.spacing, 70,
+                                 self.link.height())
+
+        self.pause_play = QPushButton("Pause/Play", self)
+        self.pause_play.clicked.connect(self.change_pause_state)
+        self.pause_play.setGeometry((previous_btn.x() + previous_btn.width()) + self.spacing, previous_btn.y(),
+                               previous_btn.width(), previous_btn.height())
+
+        self.skip_btn = QPushButton("Skip", self)
+        self.skip_btn.clicked.connect(lambda: audio_process.stop())
+        self.skip_btn.setGeometry((self.pause_play.x() + self.pause_play.width()) + self.spacing, self.pause_play.y(),
+                             self.pause_play.width(), self.pause_play.height())
+
+        previous_btn.setDisabled(True)
+        self.pause_play.setDisabled(audio_queue.empty())
+        self.skip_btn.setDisabled(audio_queue.empty())
+
+    def change_pause_state(self):
+        if not self.__paused__:
+            audio_process.pause()
+            self.__paused__ = True
+        else:
+            audio_process.unpause()
+            self.__paused__ = False
+
+    def run_audio(self):
+        """
+        Runs the audio.
+        :return:
+        """
+        import threading  # Ensures threading is imported
+        thread = threading.Thread(target=prepare_and_play, args=(self.link.text(),))
+        thread.start()
+        self.pause_play.setDisabled(False)
+        self.skip_btn.setDisabled(False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", "--gui", help="Change if the app boots with or without GUI.", type=bool)
-    while True:
-        try:
-            args = parser.parse_args()
-            if args.gui: user_interface()
-            gui_interface()
-        except Exception as e:
-            print(f"{colorama.Fore.RED}An error has occured, if this error continues to occur then open an issue in the project github. Restarting interface.\n\nError:\n{e}{colorama.Fore.RESET}")
+
+    try:
+        args = parser.parse_args()
+        if args.gui: user_interface()
+
+        App = QApplication(sys.argv)
+        window = GUIInterface(version)
+        window.show()
+        os._exit(App.exec())
+    except Exception as e:
+        print(
+            f"{colorama.Fore.RED}An error has occurred, if this error continues to occur then open an issue in the "
+            f"project github.\n\nError:\n{e}{colorama.Fore.RESET}")
